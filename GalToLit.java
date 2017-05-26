e5961c165e5ff8c7c1039a595d8d5c640a4d061c
mustafa12332 <abudalumustafa@gmail.com>
2017-05-26 11:15:25 +0300

    GalToLit improve

--- GalToLit.java
+++ GalToLit.java
@@ -2,7 +2,15 @@ public class GalToLit{
     public static void main(String args[]){
         double galons,result;
         galons=10;
-        result=galons*3.7854;
-        System.out.println("There are "+result+" liters in " +galons+" galons");
+        for(galons=1;galons<100;galons++)
+        {
+            result=galons*3.7854;
+           
+            System.out.println("There are "+result+" liters in " +galons+" galons");
+            if(galons%10==0)
+            {
+                System.out.println();
+            }
+        }
     }
 }
