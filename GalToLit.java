d156338d5d7bcc2f902513f0f6e038e84acd211c
mustafa12332 <abudalumustafa@gmail.com>
2017-05-26 10:54:18 +0300

    example and GalToLit

---
+++ GalToLit.java
@@ -0,0 +1,8 @@
+public class GalToLit{
+    public static void main(String args[]){
+        double galons,result;
+        galons=10;
+        result=galons*3.7854;
+        System.out.println("There are "+result+" liters in " +galons+" galons");
+    }
+}
