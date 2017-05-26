ac118ac860a0a794b89032ee6cac3b2e928865a8
mustafa12332 <abudalumustafa@gmail.com>
2017-05-26 11:38:08 +0300

    changed folder

---
+++ chapter1/MoonGravity.java
@@ -0,0 +1,11 @@
+public class MoonGravity
+{
+    public static void main(String args[])
+    {
+       
+        double EarthWeight=80;
+        double conversionRate=0.17;
+        double MoonWeight=EarthWeight *conversionRate;
+        System.out.println(conversionRate+"MoonWeight is "+MoonWeight);
+    }
+}
